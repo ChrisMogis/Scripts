012949c061bcb01f4432fcc18993a51e80e2f883
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-03-28 16:02:19 +0200

    Create Install_DotNet35.ps1

---
+++ Install_DotNet35.ps1
@@ -0,0 +1,16 @@
+Param(
+[Parameter(Mandatory=$true)]
+[ValidateSet("Install", "Uninstall")]
+[String[]]
+$Choose
+)
+
+If ($Choose -eq "Install")
+{
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All
+}
+
+If  ($Choose -eq "Uninstall")
+{
+Disable-WindowsOptionalFeature -Online -FeatureName 'NetFx3' -Remove -NoRestart
+}
