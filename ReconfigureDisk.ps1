21560e82bb149e296940cabad358138888836dd4
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-05-11 17:05:10 +0200

    This script removes the secondary partition from a disk and then extends the primary partition.

---
+++ ReconfigureDisk.ps1
@@ -0,0 +1,18 @@
+#   Notes 
+#   Script name: ReconfigureDisk.ps1
+#   Author:      Christopher MOGIS
+#   DateCreated: 
+#
+
+$ExtraDrive = Get-WmiObject win32_logicaldisk -Filter "DeviceID ='D:'"
+
+if ($ExtraDrive.DeviceID -eq 'D:') {
+    #xcopy $ExtraDrive.DeviceID c:\ /s /y > $env:temp\DataCopy.log
+    $DriveLetter = $ExtraDrive.DeviceID.Trim(":")
+    Remove-Partition -DriveLetter $DriveLetter -Confirm:$false
+    $MaxSize = (Get-PartitionSupportedSize -DriveLetter C).sizeMax
+    Resize-Partition -DriveLetter C -Size $MaxSize
+}
+
+else { write-host "No drive with that letter exists"
+}
