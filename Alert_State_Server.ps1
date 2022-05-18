56487bdf22a9521a5edb9723b8341fcb69a5d401
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-05-18 15:46:59 +0200

    Update Alert_State_Server.ps1
    Suppress informations

--- Alert_State_Server.ps1
+++ Alert_State_Server.ps1
@@ -23,25 +23,25 @@ $Option
     CreateLogsFolder
 
 #Declaration of script variables
-    $Client = "C-QUADRAT"
+    $Client = "Client Name"
     $Server = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
     $Date = Get-Date
     $LogPath = "C:\Logs\StateServer.log"
 
 # Define the email address to send notifications to
-    $to = "cmogis@janus-consulting.fr"
-	$to2 = "support@interfacerepublic.fr"
+    $to = "YourEmailAdress"
+	$to2 = "OtherEmailAdress"
 	
 If ($Option -eq "Reboot")
 {
 #Send the notification
     Write-Output "$($Date) Send alert email to $($to) and $($to2) : The server $Server has rebooted." | Tee-Object -FilePath $LogPath -Append
-	Send-MailMessage -To $($to;$to2) -From "AdmIR@c-quadrat.fr" -SmtpServer cquadrat-fr0c.mail.protection.outlook.com -Subject "$Client - $Server - Server Reboot" -Body "The server $Server has rebooted."
+	Send-MailMessage -To $($to;$to2) -From "SenderEmailAdress" -SmtpServer SmtpAdress -Subject "$Client - $Server - Server Reboot" -Body "The server $Server has rebooted."
 }
 
 If  ($Option -eq "Shutdown")
 {
 #Send the notification
     Write-Output "$($Date) Send alert email to $($to) and $($to2) : The server $Server has been shutdown." | Tee-Object -FilePath $LogPath -Append
-	Send-MailMessage -To $($to;$to2) -From "AdmIR@c-quadrat.fr" -SmtpServer cquadrat-fr0c.mail.protection.outlook.com -Subject "$Client - $Server - Server Shutdown" -Body "The server $Server has been shutdown."
+	Send-MailMessage -To $($to;$to2) -From "SenderEmailAdress" -SmtpServer SmtpAdress -Subject "$Client - $Server - Server Shutdown" -Body "The server $Server has been shutdown."
 }
